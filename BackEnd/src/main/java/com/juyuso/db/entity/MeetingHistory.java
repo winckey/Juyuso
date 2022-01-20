6b20051eaf3dd70aeada5ce3ba82f56a87184a80
박대언 <mem29238@gmail.com>
2022-01-20 14:35:27 +0900

    [BE] Fix: 참조 FK 수정

--- BackEnd/src/main/java/com/juyuso/db/entity/MeetingHistory.java
+++ BackEnd/src/main/java/com/juyuso/db/entity/MeetingHistory.java
@@ -19,7 +19,7 @@ public class MeetingHistory {
     private Meeting meeting;
 
     @ManyToOne
-    @JoinColumn(name = "user_id") // User 테이블의 pk
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
     private String action;
