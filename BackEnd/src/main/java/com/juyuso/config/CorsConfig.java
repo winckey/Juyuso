39182bbaee1da0bc8e4c14311b057ae78bbb888e
박대언 <mem29238@gmail.com>
2022-01-21 16:32:29 +0900

    [BE] Fix: CORS 오류 수정, 로그 설정 추가
    * mariadb 드라이버 변경
    * 로그 추가
    * CORS 오류 수정

--- BackEnd/src/main/java/com/juyuso/config/CorsConfig.java
+++ BackEnd/src/main/java/com/juyuso/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.juyuso.config;
 
+import com.juyuso.common.util.JwtTokenUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,10 +15,12 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);   // 내 서버가 응답할 때, JSON을 자바스크립트에서 처리할 수 있게 할지 설정
-        config.addAllowedOrigin("*");   // 모든 ip에 응답 허용
+        config.addAllowedOriginPattern("*");   // 모든 ip에 응답 허용
         config.addAllowedHeader("*");   // 모든 header에 응답 허용
+        config.addExposedHeader(JwtTokenUtil.HEADER_STRING);
         config.addAllowedMethod("*");   // 모든 post, get, put, delete, patch 요청을 허용
-        source.registerCorsConfiguration("/api/*", config);
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
