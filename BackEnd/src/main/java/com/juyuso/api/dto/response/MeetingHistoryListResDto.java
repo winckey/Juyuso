e3ecbcfb2c39e753ce1aa86303b5784c3bd9deb6
protossking <upgrademarine5@icloud.com>
2022-01-25 14:30:48 +0900

    [BE] Fix: Meeting Api 반환 타입 변경 및 리팩토링

--- BackEnd/src/main/java/com/juyuso/api/dto/response/MeetingHistoryListResDto.java
+++ BackEnd/src/main/java/com/juyuso/api/dto/response/MeetingHistoryListResDto.java
@@ -1,6 +1,8 @@
 package com.juyuso.api.dto.response;
 
 import com.juyuso.db.entity.MeetingHistory;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.format.DateTimeFormatter;
@@ -9,11 +11,17 @@ import java.util.List;
 
 @Getter
 @Setter
+@ApiModel("MeetingHistory List Response")
 public class MeetingHistoryListResDto {
+    @ApiModelProperty(name = "유저 닉네임", example = "냠냠")
     private String nickName;
+    @ApiModelProperty(name = "입장, 생성, 퇴장 시간", example = "20220101")
     private String insertedTime;
+    @ApiModelProperty(name = "기록타입", example = "입장, 퇴장, 생성")
     private String action;
+    @ApiModelProperty(name = "미팅 방 번호", example = "2")
     private Long meetingId;
+    @ApiModelProperty(name = "미팅 방 이름", example = "술한잔해요")
     private String meetingTitle;
 
     public MeetingHistoryListResDto() {
--- BackEnd/src/main/java/com/juyuso/api/dto/response/MeetingListResDto.java
+++ BackEnd/src/main/java/com/juyuso/api/dto/response/MeetingListResDto.java
@@ -3,6 +3,9 @@ package com.juyuso.api.dto.response;
 import com.juyuso.db.entity.HashTag;
 import com.juyuso.db.entity.Meeting;
 import com.juyuso.db.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
@@ -13,12 +16,18 @@ import java.util.function.Function;
 
 @Getter
 @Setter
+@ApiModel("Meeting List Response")
 public class MeetingListResDto {
 
+    @ApiModelProperty(name = "방 번호", example = "12")
     private Long meetingId;
+    @ApiModelProperty(name = "방 이름", example = "술먹자 ")
     private String meetingTitle;
+    @ApiModelProperty(name = "해시태그 ", example = "부산 , 20, 40")
     private List<String> hashtag = new ArrayList<>();
+    @ApiModelProperty(name = "공개방 여부", example = "true")
     private boolean common;
+
     private String nickName;
 
     public MeetingListResDto(Long meetingId, String meetingTitle,
