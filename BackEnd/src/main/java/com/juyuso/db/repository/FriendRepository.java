703ef21aa7a044a5f758be17d4a12fec85554706
JangHyunJin <winckey3@gmail.com>
2022-01-26 23:32:55 +0900

    [BE] Fix : repository update 수정

--- BackEnd/src/main/java/com/juyuso/db/repository/FriendRepository.java
+++ BackEnd/src/main/java/com/juyuso/db/repository/FriendRepository.java
@@ -7,6 +7,7 @@ import com.juyuso.db.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -19,7 +20,7 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
             "WHERE f.from_id = %:id% and u.id = f.from_id", nativeQuery = true)
     List<User> findListByUserId(@Param("id")String id);
 
-
+    @Modifying
     @Query(value = "delete FROM friend f " +
                     "WHERE from_id = :from and user_id = :to", nativeQuery = true)
     void deleteBothByUserId(long from , long to );
