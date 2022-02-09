c63b379f8089e12f7e49239291990d0bb3c50d23
protossking <upgrademarine5@icloud.com>
2022-02-09 20:12:03 +0900

    [BE] Fix: 1:1채팅 권한 설정

--- BackEnd/src/main/java/com/juyuso/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/juyuso/config/SecurityConfig.java
@@ -39,8 +39,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(corsFilter)
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService))
                 .authorizeRequests()
-//                .antMatchers("/api/v1/users/me").authenticated()       // 이 주소로 들어오면 인증이 필요하다.
-//                .antMatchers("/api/admin/**").access("hasRole('ADMIN')") // 이 주소로 들어오려면 ADMIN 권한이 필요하다.
+                .antMatchers("/api/users/login", "/api/users/id/**", "/api/users").permitAll()
+                .antMatchers("/static/res/**").permitAll()
+                .antMatchers("/ws/**").permitAll()
+                .antMatchers("/broadcast/**").permitAll()
+                .antMatchers("/api/chatRoom/**").permitAll()
+                .antMatchers("/subscribe/chat/**", "/publish/chat/message/**").permitAll()
                 .anyRequest().permitAll()
                 .and().cors();
     }
