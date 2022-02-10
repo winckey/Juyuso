49c4db99d3e3be91d8e3708a737336e9bd587537
JangHyunJin <winckey3@gmail.com>
2022-02-10 13:49:23 +0900

    [BE] Fix: 친구 검색 쿼리 수정

--- BackEnd/src/main/java/com/juyuso/api/dto/request/FriendReqDto.java
+++ BackEnd/src/main/java/com/juyuso/api/dto/request/FriendReqDto.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotBlank;
 @ToString
 public class FriendReqDto {
 
-    @NotBlank
+    //@NotBlank
     @ApiModelProperty(name = "유저/신청 ID", example = "id (pk 고유번호넘겨 주세요)")
     private Long id;
 
--- BackEnd/src/main/java/com/juyuso/db/repository/FriendRepository.java
+++ BackEnd/src/main/java/com/juyuso/db/repository/FriendRepository.java
@@ -16,9 +16,15 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     @Transactional(readOnly = true)
     @Query(value = "select f from Friend f where f.from = :from and f.to = :to")
-    Optional<Friend> findFriendByFromAndTo(@Param("from") User from, @Param("to") User to);
+    Optional<Friend> findFriendByFromAndTo2(@Param("from") User from, @Param("to") User to);
+
+    @Transactional(readOnly = true)
+    Optional<Friend> findByFromAndTo(@Param("from") User from, @Param("to") User to);
 
     @Modifying(clearAutomatically = true)
     @Query(value = "delete from Friend f where f.from.id = :from and f.to.id = :to")
     void deleteById(long from, long to);
+
+    @Modifying(clearAutomatically = true)
+    void deleteByFromIdAndToId(long from, long to);
 }
