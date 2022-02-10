803aac2b354c3ad33dc52837ec4dd8c4b12a42be
protossking <upgrademarine5@icloud.com>
2022-02-10 14:35:56 +0900

    [BE] Fix: 미팅방 쿼리 수정

--- BackEnd/src/main/java/com/juyuso/db/repository/MeetingRepository.java
+++ BackEnd/src/main/java/com/juyuso/db/repository/MeetingRepository.java
@@ -11,28 +11,24 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 
-    @Query(value = "SELECT * FROM meeting m inner join hash_tag h " +
-            "on m.meeting_id = h.meeting_id WHERE h.tag LIKE %:tag%", nativeQuery = true)
+
+    @Query("SELECT m FROM Meeting m join m.hashtags h WHERE h.tag LIKE %:tag% AND m.active = TRUE")
     Page<Meeting> findAllByTags(@Param("tag")String tags, Pageable pageable);
 
-    @Query("SELECT m FROM Meeting m WHERE m.active = true and m.title LIKE CONCAT('%',:title, '%')")
+    @Query("SELECT m FROM Meeting m WHERE m.active = TRUE AND m.title LIKE %:title%")
     Page<Meeting> findAllByTitleContaining(@Param("title") String title, Pageable pageable);
 
-    @Query(value = "SELECT * FROM meeting m LEFT JOIN user_img ui ON m.owner_id = ui.user_id WHERE m.active = TRUE ", nativeQuery = true)
+    @Query(value = "SELECT m FROM Meeting m LEFT OUTER JOIN m.owner i WHERE m.active = TRUE ")
     Page<Meeting> findAll(Pageable pageable);
 
-
-    @Query(value = "SELECT * FROM meeting m inner join hash_tag h " +
-            "on m.meeting_id = h.meeting_id WHERE h.tag LIKE %:tag% AND m.common = :common ", nativeQuery = true)
+    @Query("SELECT m FROM Meeting m join m.hashtags h WHERE h.tag LIKE %:tag% AND m.common = :common AND m.active = TRUE")
     Page<Meeting> findAllByTagContainingAndCommon(@Param("tag")String tags, Pageable pageable, @Param("common") Boolean common);
 
-    @Query("SELECT m FROM Meeting m WHERE m.active = true and m.title LIKE CONCAT('%',:title, '%') AND m.common = :common ")
+    @Query("SELECT m FROM Meeting m WHERE m.active = true AND m.title LIKE %:title% AND m.common = :common")
     Page<Meeting> findAllByTitleContainingAndCommon(@Param("title") String title, Pageable pageable, @Param("common")Boolean common);
 
-
-    @Query(value = "SELECT * FROM meeting m LEFT JOIN user_img ui ON m.owner_id = ui.user_id WHERE m.active = TRUE AND m.common = :common ", nativeQuery = true)
+    @Query("SELECT m FROM Meeting m WHERE m.active = TRUE AND m.common = :common ")
     Page<Meeting> findAllByCommon(Pageable pageable, @Param("common") Boolean common);
-
 
     @Query(value = "UPDATE Meeting m SET m.active = false",nativeQuery = true)
     void changeActiveMeeting();
