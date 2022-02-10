5d0ec5d9a2ac39a8827bea3fbf5567731bbcd43e
박대언 <mem29238@gmail.com>
2022-02-10 15:10:13 +0900

    [BE] Feat: 채팅 발송시 알람 기능 및 코드 Refactoring

--- BackEnd/src/main/java/com/juyuso/api/controller/ChatRoomController.java
+++ BackEnd/src/main/java/com/juyuso/api/controller/ChatRoomController.java
@@ -3,8 +3,12 @@ package com.juyuso.api.controller;
 import com.juyuso.api.dto.response.ChatRoomResDto;
 import com.juyuso.api.service.ChatRoomService;
 import com.juyuso.api.service.FriendService;
+import com.juyuso.common.model.response.BaseResponseBody;
 import com.juyuso.db.entity.User;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -14,23 +18,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 
-@Api(value = "채팅 방 api")
+
+@Valid
+@Api(value = "채팅 방 api", tags = {"채팅 방 관리"})
 @RestController
-@RequestMapping("/api/chatRoom")
+@RequestMapping("/api/chat/room")
 @RequiredArgsConstructor
 public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
     private final FriendService friendService;
-    ////1 채팅방 생성
-    @GetMapping("/{freindId}")
-    public ResponseEntity<ChatRoomResDto> chatHome(@ApiIgnore Authentication authentication , @PathVariable Long freindId){
 
-        System.out.println("========"+freindId);
+    @GetMapping("/{friendId}")
+    @ApiOperation(value = "채팅방 아이디 찾기")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공", response = ChatRoomResDto.class),
+            @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
+            @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
+    })
+    public ResponseEntity<ChatRoomResDto> chatHome(@ApiIgnore Authentication authentication, @Positive @PathVariable("friendId") Long friendId){
         User user = (User) authentication.getDetails();
-        User friend = friendService.getFriendInfo(freindId);
-        Long roomId = chatRoomService.getChatRoomId(user , friend) ;
+        User friend = friendService.getFriendInfo(friendId);
+        Long roomId = chatRoomService.getChatRoomId(user, friend);
 
         return ResponseEntity.ok(ChatRoomResDto.of(200, "Success", roomId));
     }
