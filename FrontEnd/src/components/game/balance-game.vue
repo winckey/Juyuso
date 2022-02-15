16c4bf59a73b1fd655120106bb6c8b000273b6ff
박수아 <tndk4997@naver.com>
2022-02-15 13:32:34 +0900

    [FE] Fix: 결과창 수정

--- FrontEnd/src/components/game/balance-game.vue
+++ FrontEnd/src/components/game/balance-game.vue
@@ -9,9 +9,9 @@
       <v-row>
         <v-col>
           <user-video class="col-md-12" style="height: 28vh" :stream-manager="publisher"/>
-          <div v-for="sub in subscribers.length" :key="sub">
+          <!-- <div v-for="sub in subscribers.length" :key="sub">
             <user-video style="height:28vh" class="col-md-12" :stream-manager="subscribers[sub-1].stream.connection.connectionId"/>{{subscribers[sub-1].stream.connection.connectionId }}
-          </div>
+          </div> -->
           <user-video style="height:28vh" class="col-md-12 camera" v-for="sub in subscribers" :key="sub.stream.connection.connectionId" :stream-manager="sub"/>
         </v-col>
         <v-col cols="4">
@@ -75,7 +75,7 @@
               </div>
               <v-dialog v-model="balanceGame.isEnd"
                 max-width="400">
-                <v-card v-if="balanceGame.cardData">
+                <v-card v-if="balanceGame.cardData" class="dialog">
                   <div
                     class="result win"
                     v-if="winCard == myPickedCard">
@@ -122,7 +122,7 @@
                   </v-progress-linear>
                   <v-row>
                     <v-col>
-                      <div class="win-message">
+                      <div class="result-box">
                         {{balanceGame.gameData[0][balanceGame.randomNum]}} 을(를) 
                         <div v-for="player in balanceGame.cardData[0]" :key="player.username">
                           {{ player.username }} 님
@@ -131,7 +131,7 @@
                       </div>
                     </v-col>
                     <v-col>
-                      <div class="win-message">
+                      <div class="result-box">
                         {{balanceGame.gameData[1][balanceGame.randomNum]}} 을(를)
                         <div v-for="player in balanceGame.cardData[1]" :key="player.username">
                           {{ player.username}} 님
@@ -160,7 +160,8 @@
                     </span>
                     님이 승리하였습니다
                   </div>
-                  <div v-if="balanceGame.cardData[0].length == balanceGame.cardData[1].length">
+                  <div v-if="balanceGame.cardData[0].length == balanceGame.cardData[1].length"
+                    class="win-messege">
                     무승부입니다
                   </div>
                 </v-card>
@@ -224,12 +225,14 @@ export default {
           '계속 자도 피곤하기','평생 여름','친구의 애인과 바람','진정한 우정','19살로 돌아가서 수능 다시 보기','똥 안먹었는데 똥 먹었다고 소문나기(뉴스특보로 나옴)','아무 일 없이 씻고 8천원 내기','50% 확률로 10억 받기','스윙칩만 8달 동안 먹기','내가 좋아하는 사람이 날 싫어하기',
           '혼자 뷔페 가기','참다참다 야밤에 먹는 라면','모르는게 약','버거킹','탄산 없는 콜라','물렁물렁한 복숭아','코털 긴 애인','소음 공해','내 애인 패션 투명 스키니진','환승 이별',
           '이성 친구와 1박하는 애인','머리카락 없는 애인','애인 하루 100번 연락','매번 술 먹고 데리러 오라는 애인','내 속옷에 친구 손','모르는 사람 집에 애인 속옷','제일 싫어하는 사람과 같이 1억 복권 당첨','추성훈 선수한테 맞고 이국종 교수한테 수술받기','감자튀김에 간장 찍어먹기','탄산 없는 탄산음료',
-          '열 손가락도 모자라 발가락에 까지 다이아 끼워주는 나 밖에 모르는 바보 이명박','한도 없는 카드를 쥐어주며 사고 싶은 거 다 사게 해주지만 집 안에서 나체로 생활하는 김경진','평생 떡볶이만 먹기','배터리 5% 데이터 가능','차가운 피자','재미없는 비행기 1시간','예쁜 글씨체 획득','이상적인 얼굴로 살기','머리 아픔','바선생 24시간 관찰'],
+          '열 손가락도 모자라 발가락에 까지 다이아 끼워주는 나 밖에 모르는 바보 이명박','한도 없는 카드를 쥐어주며 사고 싶은 거 다 사게 해주지만 집 안에서 나체로 생활하는 김경진','평생 떡볶이만 먹기','배터리 5% 데이터 가능','차가운 피자','재미없는 비행기 1시간','예쁜 글씨체 획득','이상적인 얼굴로 살기','머리 아픔','바선생 24시간 관찰',
+          '비오는날 칵테일 우산','북극에서 아아 마시기','대머리 되기'],
           ['평생 직장임 월 1000(연차없음)','토맛 토마토','10년지기 절친 생일파티','모닝에 치이기','평생동안 노출','비오는 날 새 신발','1년동안 친구 없이 살기','대장내시경 팔만번 하기','방구 뀔 때 트름소리','대학교 10년 다니기',
           '계속 먹어도 배고프기','평생 겨울','애인의 친구와 바람','진정한 사랑','그냥 이대로 살기','진짜 똥 먹었는데 아무도 모르기','사람 많은 목욕탕에서 넘어지고 8만원 받기(목욕탕 안에 모든 사람한테 주목받고 걱정 받음)','그냥 5000만원 받기','스윙스한테 800만원 주기','내가 싫어하는 사람이 날 좋아하기',
           '혼자 놀이공원 가기','등산 후 먹는 파전 + 막걸리','아는게 힘','맘스터치','치즈 없는 피자','딱딱한 복숭아','겨털 긴 애인','송해 고음','형광 핑크 망고 나시','잠수 이별',
           '전 애인과 단 둘이 술 마시는 애인','머릿속 빈 애인','한 달에 한번 연락','매번 술 먹으면 연락 두절되는 애인','친구 속옷 안에 내 손','애인집에 모르는 사람 속옷','그냥 살기','이국종 교수한테 맞고 추성훈 선수에게 수술받기','회에 케챱 찍어먹기','녹아서 액체가 된 아이스크림',
-          '묶어놓고 나 때리는 조인성','사채 끌어다쓰는 도박꾼 원빈','평생 떡볶이 안 먹기','배터리 100% 데이터 와이파이 불가','따듯하고 치즈 듬뿍 들어간 고급 피자 끝부분','재미있는 비행기 10시간','엄청난 필력 획득','1억 받기','배 아픔','자는데 같은 방에서 바선생 돌아다니기']
+          '묶어놓고 나 때리는 조인성','사채 끌어다쓰는 도박꾼 원빈','평생 떡볶이 안 먹기','배터리 100% 데이터 와이파이 불가','따듯하고 치즈 듬뿍 들어간 고급 피자 끝부분','재미있는 비행기 10시간','엄청난 필력 획득','1억 받기','배 아픔','자는데 같은 방에서 바선생 돌아다니기',
+          '모르는 사람 우산 안으로 살포시 들어가기','참숯불 황토 가마에서 뜨아 마시기','침팬치 되기']
         ],
         randomNum : 0
       },
@@ -260,7 +263,7 @@ export default {
         this.balanceGame.cardData = [[], []],
         this.balanceGame.totalTime = 5,
         this.balanceGame.isStart = true
-        const random = this.makeRandomNum(0, 49)
+        const random = this.makeRandomNum(0, 52)
         this.balanceGame.randomNum = random
         this.sendGameInfo()
         console.log('랜덤 번호(this.random)',this.random)
@@ -388,6 +391,13 @@ export default {
     top: 10%;
     right: 40%;
 } */
+.dialog{
+  overflow:scroll;
+  -ms-overflow-style: none;
+}
+::-webkit-scrollbar{
+  display: none;
+}
 .camera{
   margin: 0%;
   padding: 0%;
@@ -456,6 +466,15 @@ export default {
 .win-messege{
   text-align: center;
   padding: 20px;
+}
+.result-box{
+  height: 100px;
+  text-align: center;
+  /* border: 1px solid black; */
+  border-radius: 10px;
+  background-color: rgb(245, 241, 225);
+  vertical-align: middle;
+  padding-top: 10px;
 }
 .result{
   text-align: center;
