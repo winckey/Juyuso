f5ebbee863047ecc14ff8af32e77b760e1a4f48d
JangHyunJin <winckey3@gmail.com>
2022-02-20 10:42:01 +0000

    edit : chatmessagetest

--- BackEnd/src/test/java/com/juyuso/Chat/ChatMessageServiceTests.java
+++ BackEnd/src/test/java/com/juyuso/Chat/ChatMessageServiceTests.java
@@ -1,35 +1,84 @@
 package com.juyuso.Chat;
 
+import com.juyuso.api.dto.request.chat.ChatMessageReqDto;
+import com.juyuso.api.service.chat.ChatMessageService;
+import com.juyuso.api.service.chat.ChatMessageServiceImpl;
 import com.juyuso.db.entity.chat.ChatRoom;
 import com.juyuso.db.entity.chat.ChatRoomUser;
+import com.juyuso.db.entity.chat.Message;
+import com.juyuso.db.entity.user.User;
+import com.juyuso.db.repository.chat.ChatMessageRepository;
+import com.juyuso.db.repository.chat.ChatRoomRepository;
+import com.juyuso.db.repository.user.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.data.domain.Pageable;
+
+import javax.validation.constraints.NotNull;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 
 class ChatMessageServiceTests {
+
+
+    @Mock
+    ChatRoomRepository chatRoomRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    ChatMessageRepository chatMessageRepository;
+
+    @Mock
+    Pageable pageable;
+
+    @Mock
+    Message message;
 
     @Test
     void contextLoads() {
     }
 
-    @DisplayName("유저 채팅방 입장")
+    @DisplayName("채팅 생성")
     @Test
     public void setChatRoom() {
 
         //given
-        ChatRoom newChatRoom = new ChatRoom();
-        ChatRoomUser chatRoomUser = new ChatRoomUser();
+        ChatMessageReqDto chatMessageReqDto = new ChatMessageReqDto("1" , "1" ,"name" , "message" , "2");
+        //given
+        ChatMessageService chatMessageService = new ChatMessageServiceImpl(chatRoomRepository , userRepository , chatMessageRepository);
+
+
         //when
 
-        chatRoomUser.setChatRoom(newChatRoom);
+        chatMessageService.saveMessage(chatMessageReqDto);
+
 
         //then
-        assertEquals(chatRoomUser.getChatRoom() , newChatRoom);
-        assertEquals(newChatRoom.getChatRoomUsers().get(0) , chatRoomUser);
+        then(chatMessageRepository).should(times(1)).getById(any());
+        then(message).should(times(1)).builder();
+
     }
+    
+    @DisplayName("채팅리스트 확인")
+    @Test
+    public void getMessageList() {
+
+        //given
+        ChatMessageService chatMessageService = new ChatMessageServiceImpl(chatRoomRepository , userRepository , chatMessageRepository);
 
 
+        //when
 
+        chatMessageService.getMessageList(1L , pageable);
+
+        //then
+        then(chatMessageRepository).should(times(1)).findAllByChatRoomId(any(),any());
+    }
 }
